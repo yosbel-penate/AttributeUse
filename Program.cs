52e20da79407c0d686ccb32f65e28c2ea49bf2d5
yosbel-penate <58090876+yosbel-penate@users.noreply.github.com>
2020-09-13 22:48:29 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributeUse
+{
+    public class LocalizedDisplayNameAttribute : DisplayNameAttribute
+    {
+
+
+        public LocalizedDisplayNameAttribute(string resourceId)
+            : base(GetMessageFromResource(resourceId))
+        {
+          
+        }
+
+        private static string GetMessageFromResource(string resourceId)
+        {
+            List<string> cd = new List<string>
+            {
+                "hola",
+                "pepe",
+                "mama"
+            };
+            return cd[int.Parse(resourceId)];
+        }
+    }
+    class person
+    {
+        const string _attname="none";
+        string name;
+
+        public static string Noo()
+        {
+            return "none";
+        }
+        public person()
+        {
+            Name = name;
+        }
+
+        //[DisplayName(person.Noo())]
+        [LocalizedDisplayNameAttribute("1")]
+        public string Name { get => name; set => name = value; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
